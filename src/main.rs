a60cf5a8b6bee7d4efac8490d1b2f35c6e7ecf80
Leonard <remon.w.dev@gmail.com>
2023-09-24 23:46:48 -0400

    my god what is this

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+
 mod constants;
 
 use std::process::Command;
 
+fn curlPage(letter: char, offset: u32) -> Vec<u8> {
+	println!("{}?ciave={}&offset={}", constants::constants::baseUrl, letter, offset);
 
-fn main() {
 	let scrape = Command::new("curl")
-		.arg(constants::constants::baseUrl)
+		.arg(format!("{}?ciave={}&offset={}", constants::constants::baseUrl, letter, offset))
 		.output()
 		.expect("failed to execute process");
-	
-	let str_ = String::from_utf8(scrape.stdout).unwrap();
 
-	println!("{}", str_.as_str());
+	return scrape.stdout;
+}
+
+
+fn main() {
+	let binding = String::from_utf8(curlPage('a', 2000)).unwrap();
+	let html = binding.as_str();
+
+	let dom = tl::parse(html, tl::ParserOptions::default()).unwrap();
+
+
+	// find the navigation box at the end of the page
+	
+	let mut navBox = dom.get_elements_by_class_name("navBox");
+
+	let parser = dom.parser();
+
+	loop {
+		let elem = navBox.next();
+
+		if elem.is_none() {
+			break;
+		}
+
+
+		let tag = elem.unwrap().get(parser).unwrap();
+
+		println!("{}", tag.inner_text(parser))
+	}
+	
 }
