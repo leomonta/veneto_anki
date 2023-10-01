3b2a4f5f084e0a95356e03f94461b4f44563e380
Leonard <remon.w.dev@gmail.com>
2023-10-01 01:08:54 -0400

    added some constants and functions

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,11 @@
-//pub mod constants {
+pub const baseUrl: &str = "http://www.elgalepin.com/linguaveneta-vec.asp";
+pub const ciave: &str = "ciave";
+pub const offset: &str = "offset";
+pub const wordUrl: &str = "http://www.elgalepin.com/linguaveneta-detalio.asp";
 
-	pub const baseUrl: &str = "http://www.elgalepin.com/linguaveneta-vec.asp";
-	pub const ciave: &str = "ciave";
-	pub const offset: &str = "offset";
-//}
+pub struct Word {
+	word: String,
+	ID: u32,
+}
+
+pub const letters: [&str; 27] = ["a", "b", "c", "d", "dh", "e", "f", "g", "gn", "h", "i", "j", "l", "m", "n", "o", "p", "r", "s", "sh", "t", "u", "v", "x", "ç", "z", "zh"];
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,47 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
+// HAHAHAHAHAHAHAHAHA I'll keep my parethesis
+#![allow(unused_parens)]
+
 mod constants;
 
 use std::process::Command;
 
+/**
+ * curl the dictionary page with the given letter and offset
+ *
+ * each dictionary page contains 20 words the offset count the amount of words, so, to skip a page
+ * an offset of 20 is needed
+ *
+ * and obviously an offset of n*20 -> skip n pages
+ */
 fn curlPage(letter: char, offset: u32) -> Vec<u8> {
-	println!("{}?{}={}&{}={}", constants::baseUrl, constants::ciave, letter, constants::offset, offset);
+	let requestStr = format!("{}?{}={}&{}={}", constants::baseUrl, constants::ciave, letter, constants::offset, offset);
+	println!("{}", requestStr);
 
 	let scrape = Command::new("curl")
-		.arg(format!("{}?ciave={}&offset={}", constants::baseUrl, letter, offset))
+		.arg(requestStr)
 		.output()
 		.expect("failed to execute process");
 
 	return scrape.stdout;
 }
 
-
-fn main() {
-	let binding = String::from_utf8(curlPage('a', 2000)).unwrap();
-	let html = binding.as_str();
+/**
+ * Analyzing the dom fetches all of the words and relative pages present
+ * 
+ * also returns the amount of worded added to the given array, if < 20 it means that this is the
+ * last page for the given letter
+ *
+ * @param html the html string to parse
+ * @param vec the word vector to add the words to
+ *
+ * @return the amount of words found and added to the array 0 <= n <= 20
+ */
+fn appendWords(html: &str, mut vec: Vec<constants::Word>) -> u8 {
 
 	let dom = tl::parse(html, tl::ParserOptions::default()).unwrap();
-
 
 	// find the navigation box at the end of the page
 	
@@ -41,6 +60,25 @@ fn main() {
 		let tag = elem.unwrap().get(parser).unwrap();
 
 		println!("{}", tag.inner_text(parser))
+	}
+
+	return false;
+}
+
+
+fn main() {
+
+	let mut array: Vec<String>;
+
+	loop {
+		let htmlOwner = String::from_utf8(curlPage('a', 2000)).unwrap();
+		println!("Continuing");
+
+		appendWords();
+
+		if (!canContinue(htmlOwner.as_str())) {
+
+		}
 	}
 	
 }
